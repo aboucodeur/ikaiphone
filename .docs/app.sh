6816d2efcf828c5a60197e880e350bb00c0acb4b
codeurabou <codeurabou123@gmail.com>
2024-05-30 18:26:53 +0000

    add executable mac

--- .docs/app.sh
+++ .docs/app.sh
@@ -5,6 +5,17 @@ if ! type "php" > /dev/null; then
   echo "PHP n'est pas installé. Veuillez installer PHP pour continuer."
   exit 1
 fi
+
 # Lancez le serveur web de l'application
 cd /Users/pommestore/abousoft
-php artisan serve
+php artisan serve &
+
+# Création d'une icône sur le bureau pour MacOS
+echo "[Desktop Entry]
+Type=Application
+Name=Abousoft Server
+Exec=/Users/pommestore/abousoft/app.sh
+Icon=/Users/pommestore/abousoft/icon.png
+Terminal=true" > ~/Desktop/AbousoftServer.desktop
+
+chmod +x ~/Desktop/AbousoftServer.desktop
